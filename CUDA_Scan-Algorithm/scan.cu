92b0d1a7067a8cba1ddf772accdb9b94e539b920
Amin Mansouri <49798286+amansouri3476@users.noreply.github.com>
2020-08-18 18:48:52 +0430

    Add files via upload

---
+++ CUDA_Scan-Algorithm/scan.cu
@@ -0,0 +1,22 @@
+// ONLY MODIFY THIS FILE
+
+#include "scan.h"
+#include "gpuerrors.h"
+
+#define tx threadIdx.x
+#define ty threadIdx.y
+#define tz threadIdx.z
+
+#define bx blockIdx.x
+#define by blockIdx.y
+#define bz blockIdx.z
+
+// you may define other parameters here!
+// you may define other macros here!
+// you may define other functions here!
+
+void gpuKernel(int* a, int* c,int n) {	
+
+
+
+ }
